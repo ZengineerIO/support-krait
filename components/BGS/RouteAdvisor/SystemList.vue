265431b16f4d1ad17c982a9420bd55fdb6f7d211
Zengineer <zengineer@endocluster.com>
2021-04-06 16:36:34 +0200

    added mode selection for system selection. This actually allows mobile usage. Added Infos to How To.

--- components/BGS/RouteAdvisor/SystemList.vue
+++ components/BGS/RouteAdvisor/SystemList.vue
@@ -12,13 +12,22 @@
     </b-row>
     <b-row>
       <b-button-group class="w-100 mx-3 mb-2">
-        <b-button variant="outline-info" @click="selectAll"
+        <b-button class="w-25" variant="outline-info" @click="selectAll"
           >Select All</b-button
         >
-        <b-button variant="outline-info" @click="deselectAll"
+        <b-button class="w-25" variant="outline-info" @click="deselectAll"
           >Deselect All</b-button
         >
-        <b-button v-b-modal="'bgs-system-advisor-how-to'" variant="outline-info"
+        <b-button class="w-25" variant="outline-info" @click="toggleSelectMode"
+          >Mode:
+          {{
+            selectMode.charAt(0).toUpperCase() + selectMode.slice(1)
+          }}</b-button
+        >
+        <b-button
+          v-b-modal="'bgs-system-advisor-how-to'"
+          class="w-25"
+          variant="outline-info"
           >How To</b-button
         >
       </b-button-group>
@@ -39,12 +48,19 @@
           All).
         </p>
         <p>
+          <strong>Range Mode:</strong>
           Use <strong>SHIFT</strong> or <strong>CTRL</strong> to select the
-          systems.
+          systems. <br />
+          This mode does not work on mobile devices.
         </p>
         <p>
-          <strong>Note:</strong> When sorting the table with systems already
-          selected, the selection will be lost!
+          <strong>Multi Mode:</strong>
+          Simply click to (de)select a system.
+        </p>
+        <p>
+          <strong>Note:</strong> When sorting the table or changing the mode
+          with systems already selected, the selection will be
+          <strong>lost</strong>!
         </p>
       </b-modal>
     </b-row>
@@ -64,7 +80,7 @@
           borderless
           dark
           sticky-header="70vh"
-          select-mode="range"
+          :select-mode="selectMode"
           primary-key="system_name"
           @row-selected="onRowSelected"
         ></b-table>
@@ -80,6 +96,7 @@ export default {
     return {
       sortBy: 'influence',
       sortDesc: true,
+      selectRange: true,
       fields: [
         { key: 'system_name', sortable: true },
         {
@@ -108,6 +125,9 @@ export default {
         this.$store.state.bgs.routeAdvisor.factionLastUpdate
       )
     },
+    selectMode() {
+      return this.selectRange ? 'range' : 'multi'
+    },
   },
   methods: {
     onRowSelected(systems) {
@@ -118,6 +138,9 @@ export default {
     },
     deselectAll() {
       this.$refs.selectableTable.clearSelected()
+    },
+    toggleSelectMode() {
+      this.selectRange = !this.selectRange
     },
   },
 }
